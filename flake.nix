265d5217486e2c2a301c80cba7e81388edc680bc
link2xt <link2xt@testrun.org>
2024-03-31 23:54:10 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        devShell = pkgs.mkShell {
+          packages = [ pkgs.diffoscope pkgs.python3 ];
+        };
+      }
+    );
+}
